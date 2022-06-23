d2a5db65a1ea3bb0f9e3179291d894c04bc7523d
kimlong0601 <kimlongt40@gmail.com>
2022-06-23 11:29:46 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './assets/boxicons-2.1.2/css/boxicons.min.css';
 import './sass/index.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <App />
   </React.StrictMode>,
 document.getElementById('root')
